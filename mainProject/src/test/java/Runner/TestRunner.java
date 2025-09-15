26600932a1346627a4dd9a6ab3964c0c1ca84557
swargam.ramya11 <swargam.ramya11.com>
2025-09-15 14:19:16 +0530

    Code

--- mainProject/src/test/java/Runner/TestRunner.java
+++ mainProject/src/test/java/Runner/TestRunner.java
@@ -13,7 +13,7 @@ import static Utilities.ReusableMethods.*;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"Utilities", "StepDefinations"},
-        tags = "@test1",
+        tags = "@Regression",
         plugin = {"pretty", "html:target/cucumber-reports",
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
@@ -22,6 +22,12 @@ import static Utilities.ReusableMethods.*;
 )
 
 public class TestRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios(){
+        return super.scenarios();
+    }
 
     @Test(
             groups = {"cucumber"},
